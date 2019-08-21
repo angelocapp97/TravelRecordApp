275a7848856509233adab482f7302731183e0af9
Angelo Cappelletti <a.cappelletti@goproject.it>
2019-08-21 18:55:24 +0200

    Implemented request permissions

--- TravelRecordApp.Android/MainActivity.cs
+++ TravelRecordApp.Android/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using System.IO;
+using Plugin.Permissions;
 
 namespace TravelRecordApp.Droid
 {
@@ -24,15 +25,20 @@ namespace TravelRecordApp.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Xamarin.FormsMaps.Init(this, savedInstanceState);
 
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
+
             string dbName = "travel_db.sqlite";
             string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             string fullPath = Path.Combine(folderPath, dbName);
 
             LoadApplication(new App(fullPath));
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
--- TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/MapPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
@@ -10,6 +11,34 @@ namespace TravelRecordApp
         public MapPage()
         {
             InitializeComponent();
+
+            GetPermissions();
+        }
+
+        private async void GetPermissions()
+        {
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.LocationWhenInUse))
+                        await DisplayAlert("Need your location", "We need to access your location", "Ok");
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.LocationWhenInUse);
+                    if (results.ContainsKey(Permission.LocationWhenInUse))
+                        status = results[Permission.LocationWhenInUse];
+                }
+
+                if (status == PermissionStatus.Granted)
+                    locationsMap.IsShowingUser = true;
+                else
+                    await DisplayAlert("Location denied", "You didn't give us permission to access location, so we can't show your location", "Ok");
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
         }
     }
 }
