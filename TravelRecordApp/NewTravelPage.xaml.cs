4c8ba3fc40cf40d618063d3ed75d04da851f05b8
Angelo Cappelletti <a.cappelletti@goproject.it>
2019-08-21 16:28:50 +0200

    Implemented using directive in sqlite connection

--- TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/NewTravelPage.xaml.cs
@@ -20,15 +20,16 @@ namespace TravelRecordApp
                 Experience = experienceEntry.Text
             };
 
-            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
-            conn.CreateTable<Post>();
-            int rows = conn.Insert(post);
-            conn.Close();
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                int rows = conn.Insert(post);
 
-            if (rows > 0)
-                DisplayAlert("Success", "Experience inserted successfully", "Ok");
-            else
-                DisplayAlert("Error", "No experience inserted", "Ok");
+                if (rows > 0)
+                    DisplayAlert("Success", "Experience inserted successfully", "Ok");
+                else
+                    DisplayAlert("Error", "No experience inserted", "Ok");
+            }
         }
     }
 }
