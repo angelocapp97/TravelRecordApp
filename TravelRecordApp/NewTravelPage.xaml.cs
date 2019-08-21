6c019920efd1fbb2e5fcac277e12434c27dadd83
Angelo Cappelletti <a.cappelletti@goproject.it>
2019-08-21 16:13:20 +0200

    Implemented new experience insert into db

--- TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/NewTravelPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using SQLite;
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
@@ -10,6 +11,24 @@ namespace TravelRecordApp
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post
+            {
+                Experience = experienceEntry.Text
+            };
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+            int rows = conn.Insert(post);
+            conn.Close();
+
+            if (rows > 0)
+                DisplayAlert("Success", "Experience inserted successfully", "Ok");
+            else
+                DisplayAlert("Error", "No experience inserted", "Ok");
         }
     }
 }
