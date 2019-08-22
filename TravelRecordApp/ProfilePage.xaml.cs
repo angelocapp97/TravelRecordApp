4ecaa25150994a2b2f9e9a663d8703d9ffd45ce9
Angelo Cappelletti <a.cappelletti@goproject.it>
2019-08-22 17:32:18 +0200

    Setup profile page

--- TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/ProfilePage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using SQLite;
+using System.Linq;
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
@@ -10,6 +12,38 @@ namespace TravelRecordApp
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using(SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = conn.Table<Post>().ToList();
+
+                var categories = postTable
+                    .OrderBy(x => x.CategoryId)
+                    .Select(x => x.CategoryName)
+                    .Distinct().ToList();
+
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+                foreach(var category in categories)
+                {
+                    var count = postTable
+                        .Where(x => x.CategoryName == category)
+                        .ToList().Count();
+
+                    if (category == null)
+                        categoriesCount.Add(string.Empty, count);
+                    else
+                        categoriesCount.Add(category, count);
+                }
+
+                categoriesListView.ItemsSource = categoriesCount;
+
+                postCountLabel.Text = postTable.Count.ToString();
+            }
         }
     }
 }
